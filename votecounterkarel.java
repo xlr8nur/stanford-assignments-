2db7acab424b733bff1a13a22192484ef8a8bac5
nur <97341887+thecodingliva@users.noreply.github.com>
2023-05-21 16:32:25 +0300

    Create votecounterkarel.java

---
+++ votecounterkarel.java
@@ -0,0 +1,49 @@
+/**
+* The VoteCounterKarel subclass cleans out the chad from
+* a ballot as described in the section handout.
+*/
+
+import stanford.karel.*;
+
+public class VoteCounterKarel extends SuperKarel {
+        public void run () {
+                while(frontIsClear)) {
+                      move();
+                      if(noBeeperPresent())
+                            removeChad();
+                }
+                move();
+} 
+
+/**
+* Removes any chad from a ballot, which consists of beepers
+* in the squares to right and left of Karel's current position.
+*/
+
+private void removeChad() {
+        turnLeft();
+        checkCorner();
+        checkCorner();
+        turnRight();
+}  
+
+/**
+* If there is a beeper in that position, Karel must assume that the
+voter did not intend to cast a vote in that column and move on to the next. If there is no
+beeper in the center square, Karel must check the other two squares in the ballot and
+remove any and all beepers so that the ballot can be counted correctly. */
+
+private void checkCorner() {
+        move();
+        while(beeperPresent())
+                pickBeeper();
+        turnAround();
+        move();
+}
+
+}
+
+
+
+
+
